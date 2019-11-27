044466c564441a414749beb8cc9989d6461f526c
fernando orozco <fernando.orozco@hotmail.fr>
2019-11-27 21:28:32 +0100

    map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,10 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { loadDynamicBannerText } from '../components/banner';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
 loadDynamicBannerText();
+initMapbox();
+
+
