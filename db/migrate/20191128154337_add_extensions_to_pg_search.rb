8fe6cd6ce8bcd5307a483192871e9e79885e2de0
tafsut abd <tafsut.a@gmail.com>
2019-11-28 16:52:58 +0100

    db:migrate réalisé après ajout migration  AddExtensionsToPgSearch

--- db/migrate/20191128154337_add_extensions_to_pg_search.rb
+++ db/migrate/20191128154337_add_extensions_to_pg_search.rb
@@ -1,4 +1,11 @@
 class AddExtensionsToPgSearch < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+    execute "CREATE EXTENSION IF NOT EXISTS unaccent;"
+  end
+
+  def down
+    execute "DROP EXTENSION IF EXISTS pg_trgm;"
+    execute "DROP EXTENSION IF EXISTS unaccent;"
   end
 end
